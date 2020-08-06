a5c3306d050be2bfe3f4cad1046d9d09090892b6
CarlosRPEvertsz <carlosrpevertsz@gmail.com>
2020-08-05 21:22:52 -0400

    Add project files.

---
+++ Tarea2-4_RegistroDatos/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea2_4_RegistroDatos.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CapturaDatosClientes()
+        {
+            return View();
+        }
+        public ActionResult RegistroDatos()
+        {
+            string nombre = Request.Form["nombre"].ToString();
+            string cedula = Request.Form["cedula"].ToString();
+            string telefono = Request.Form["telefono"].ToString();
+            string correo = Request.Form["correo"].ToString();
+
+            Models.clsDatosClientes datosClientes = new Models.clsDatosClientes();
+            datosClientes.RegistrarCliente(nombre, cedula, telefono, correo);
+            
+            return View();
+        }
+        public ActionResult ReporteDatosClientes()
+        {
+            Models.clsDatosClientes datosClientes = new Models.clsDatosClientes();
+            string TodosLosClientes = datosClientes.LeerClientes();
+            ViewData["RelacionClientes"] = TodosLosClientes;
+            return View();
+        }
+    }
+}
---
+++ Tarea2-4_RegistroDatos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tarea2_4_RegistroDatos.MvcApplication" Language="C#" %>
